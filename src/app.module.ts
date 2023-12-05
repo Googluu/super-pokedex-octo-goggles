2554d18721c8db04dc85875e81b77611bbdb57f2
sneyder <sneyder.h.rodriguez@gmail.com>
2023-12-05 13:58:09 -0500

    pokedex

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { join } from 'path';
+
+import { Module } from '@nestjs/common';
+
+import { config } from 'dotenv';
+config();
+
+import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
+
+import { PokemonModule } from './pokemon/pokemon.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      dbName: 'pokedex'
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    PokemonModule,
+  ],
+})
+export class AppModule {}
